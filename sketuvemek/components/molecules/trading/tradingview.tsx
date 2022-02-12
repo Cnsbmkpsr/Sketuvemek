c6d4262fab058e77c46e3a4d6ea81f48290adae1
Marvyn <m.aboulicam@eenov.com>
2022-02-12 18:24:04 +0100

    Implement tailwind purge

--- sketuvemek/components/molecules/trading/tradingview.tsx
+++ sketuvemek/components/molecules/trading/tradingview.tsx
@@ -1,3 +1,15 @@
-import React from 'react';
+import React from "react";
 
+import { ITradingViewProps } from "./ITradingView";
 
+const TradingView = (): JSX.Element => {
+  return (
+    <>
+      <div>
+        leo 
+      </div>
+    </>
+  )
+}
+
+export default TradingView;
