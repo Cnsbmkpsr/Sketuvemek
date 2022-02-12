31a031b91ab8fe7e7697fd4bc64250486985a3f5
Marvyn <m.aboulicam@eenov.com>
2022-02-12 18:39:48 +0100

    Implement infrastructure for app page

--- sketuvemek/src/App.tsx
+++ sketuvemek/src/App.tsx
@@ -2,24 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TradingView from './components/molecules/trading/tradingview';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App bg-gray-500 h-screen w-screen grid grid-cols-12">
+      <div className="grid-cols-8">
+        <TradingView/>
+      </div>
+      <div className="grid-cols-4">
+        
+      </div>
+    </div> 
   );
 }
 
